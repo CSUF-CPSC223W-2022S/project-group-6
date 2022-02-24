51a2317eb9b8f073e80326a22b9ef1be119930d4
Marco Gabriel <marcog10@csu.fullerton.edu>
2022-02-23 19:29:51 -0800

    currency

---
+++ Project_Group_6/currency.swift
@@ -0,0 +1,16 @@
+//
+//  currency.swift
+//  Project_Group_6
+//
+//  Created by Marco Gabriel on 2/23/22.
+//
+
+import Foundation
+struct currency {
+    var money: Int
+    var language: String
+    
+    //code
+    
+    //something
+}
