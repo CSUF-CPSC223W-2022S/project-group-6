3f6412bb345a2453860b8b0c58fa6e0545883e5b
Diego Barrales <diego.barrales@csu.fullerton.edu>
2022-04-25 20:33:09 -0700

    finished the all of the airline in my airplane database, E---X airlines are just randomized based on the the airlines I did research on

--- Project_Group_6/Feature Seat Map/Airplanes/airplaneData.swift
+++ Project_Group_6/Feature Seat Map/Airplanes/airplaneData.swift
@@ -198,6 +198,310 @@ func loadAirplanes() -> [String: [plane]] {
             plane("EJR-175", .eDomestic, "erj175"),
             plane("CRJ-200", .eDomestic, "crj200"),
             plane("CJR-700", .eDomestic, "crj700")
+        ],
+        "EI AI Israel Airlines": [
+            plane("Airbus A321", .eDomestic, "airbusA321"),
+            plane("Airbus A330-300", .eInternational, "airbusA330"),
+            plane("Airbus A350", .eInternational, "airbusA350"),
+            plane("Boeing 737-800", .eDomestic, "boeing737"),
+            plane("Boeing 777", .eInternational, "boeing777")
+        ],
+        "Emirates": [
+            plane("Boeing 737-800", .eDomestic, "boeing737"),
+            plane("787-9 Dreamliner", .eInternational, "dreamliner787")
+        ],
+        "Etihad Airways": [
+            plane("Airbus A319", .eDomestic, "airbusA319"),
+            plane("Airbus A320", .eDomestic, "airbusA320"),
+            plane("Airbus A321", .eDomestic, "airbusA321"),
+            plane("Boeing 737-800", .eDomestic, "boeing737"),
+            plane("Boeing 777", .eInternational, "boeing777"),
+            plane("Mitsubishi", .eDomestic, "crj200"),
+            plane("De Havilland", .eDomestic, "dash400")
+        ],
+        "EVA Air": [
+            plane("EJR-145", .eDomestic, "erj145"),
+            plane("EJR-175", .eInternational, "ejr175"),
+            plane("CJR-700", .eDomestic, "crj700")
+        ],
+        "Fiji Airways": [
+            plane("Airbus A319", .eDomestic, "airbusA319"),
+            plane("Airbus A320", .eDomestic, "airbusA320"),
+            plane("Airbus A321", .eDomestic, "airbusA321"),
+            plane("Airbus A330-300", .eInternational, "airbusA330"),
+            plane("787-9 Dreamliner", .eInternational, "dreamliner787"),
+            plane("ATR", .eDomestic, "atr")
+        ],
+        "Finnair": [
+            plane("Airbus A321", .eDomestic, "airbusA321"),
+            plane("Boeing 737-800", .eDomestic, "boeing737"),
+            plane("Boeing 777", .eInternational, "boeing777"),
+            plane("Airbus A350", .eInternational, "airbusA350"),
+            plane("Sukhoi Superjet 100", .eDomestic, "sukhoijet")
+        ],
+        "Hainan Airlines": [
+            plane("Boeing 747", .eInternational, "boeing747"),
+            plane("Boeing 777", .eInternational, "boeing777"),
+            plane("Airbus A320", .eDomestic, "airbusA320"),
+            plane("Airbus A321", .eDomestic, "airbusA321"),
+            plane("EJR-195", .eDomestic, "erj195")
+        ],
+        "Hawaiian Airlines": [
+            plane("Airbus A319", .eDomestic, "airbusA319"),
+            plane("Airbus A320", .eDomestic, "airbusA320"),
+            plane("Airbus A321", .eDomestic, "airbusA321"),
+            plane("Boeing 737-800", .eDomestic, "boeing737"),
+            plane("Boeing 777", .eInternational, "boeing777"),
+            plane("CJR-700", .eDomestic, "crj700"),
+            plane("EJR-175", .eInternational, "ejr175")
+        ],
+        "Horizon Air": [
+            plane("Airbus A319", .eDomestic, "airbusA319"),
+            plane("Airbus A320", .eDomestic, "airbusA320"),
+            plane("Airbus A321", .eDomestic, "airbusA321"),
+            plane("Airbus A330-300", .eInternational, "airbusA330"),
+            plane("787-9 Dreamliner", .eInternational, "dreamliner787"),
+            plane("ATR", .eDomestic, "atr")
+        ],
+        "Iberia Airlines": [
+            plane("Airbus A319", .eDomestic, "airbusA319"),
+            plane("Airbus A320", .eDomestic, "airbusA320"),
+            plane("Airbus A321", .eDomestic, "airbusA321"),
+            plane("Airbus A330-300", .eInternational, "airbusA330"),
+            plane("Airbus A350", .eInternational, "airbusA350"),
+            plane("Boeing 737-800", .eDomestic, "boeing737"),
+            plane("Boeing 767", .eDomestic, "boeing767"),
+            plane("Boeing 777", .eInternational, "boeing777"),
+            plane("Boeing 787", .eInternational, "boeing787")
+        ],
+        "InterJet": [
+            plane("Airbus A321", .eDomestic, "airbusA321"),
+            plane("Boeing 737-800", .eDomestic, "boeing737"),
+            plane("Boeing 777", .eInternational, "boeing777"),
+            plane("Airbus A350", .eInternational, "airbusA350"),
+            plane("Sukhoi Superjet 100", .eDomestic, "sukhoijet")
+        ],
+        "Japan Airlines": [
+            plane("Airbus A319", .eDomestic, "airbusA319"),
+            plane("Airbus A320", .eDomestic, "airbusA320"),
+            plane("Airbus A321", .eDomestic, "airbusA321"),
+            plane("Boeing 737-800", .eDomestic, "boeing737"),
+            plane("Boeing 777", .eInternational, "boeing777"),
+            plane("Mitsubishi", .eDomestic, "crj200"),
+            plane("De Havilland", .eDomestic, "dash400")
+        ],
+        "JetBlue": [
+            plane("EJR-145", .eDomestic, "erj145"),
+            plane("EJR-175", .eInternational, "ejr175"),
+            plane("CJR-700", .eDomestic, "crj700")
+        ],
+        "KLM Royal Dutch Airlines": [
+            plane("Boeing 747", .eInternational, "boeing747"),
+            plane("Boeing 777", .eInternational, "boeing777"),
+            plane("Airbus A320", .eDomestic, "airbusA320"),
+            plane("Airbus A321", .eDomestic, "airbusA321"),
+            plane("EJR-195", .eDomestic, "erj195")
+        ],
+        "Korean Airlines": [
+            plane("Airbus A319", .eDomestic, "airbusA319"),
+            plane("Airbus A320", .eDomestic, "airbusA320"),
+            plane("Airbus A321", .eDomestic, "airbusA321"),
+            plane("Airbus A330-300", .eInternational, "airbusA330"),
+            plane("Airbus A350", .eInternational, "airbusA350"),
+            plane("Boeing 737-800", .eDomestic, "boeing737"),
+            plane("Boeing 767", .eDomestic, "boeing767"),
+            plane("Boeing 777", .eInternational, "boeing777"),
+            plane("Boeing 787", .eInternational, "boeing787")
+        ],
+        "LATAM": [
+            plane("Boeing 737-800", .eDomestic, "boeing737"),
+            plane("787-9 Dreamliner", .eInternational, "dreamliner787")
+        ],
+        "Level Airlines": [
+            plane("Airbus A321", .eDomestic, "airbusA321"),
+            plane("Boeing 737-800", .eDomestic, "boeing737"),
+            plane("Boeing 777", .eInternational, "boeing777"),
+            plane("Airbus A350", .eInternational, "airbusA350"),
+            plane("Sukhoi Superjet 100", .eDomestic, "sukhoijet")
+        ],
+        "LOT Polish": [
+            plane("EJR-145", .eDomestic, "erj145"),
+            plane("EJR-175", .eInternational, "ejr175"),
+            plane("CJR-700", .eDomestic, "crj700")
+        ],
+        "Lufthansa": [
+            plane("Boeing 737-800", .eDomestic, "boeing737"),
+            plane("Boeing 737-800", .eInternational, "boeing737")
+        ],
+        "Mokulele Airlines": [
+            plane("Airbus A321", .eDomestic, "airbusA321"),
+            plane("Airbus A330-300", .eInternational, "airbusA330"),
+            plane("Airbus A350", .eInternational, "airbusA350"),
+            plane("Boeing 737-800", .eDomestic, "boeing737"),
+            plane("Boeing 777", .eInternational, "boeing777")
+        ],
+        "Nippon Cargo Airlines": [
+            plane("Boeing 737-800", .eDomestic, "boeing737"),
+            plane("Boeing 737-800", .eInternational, "boeing737"),
+        ],
+        "Norwegian Air Shuttle": [
+            plane("Airbus A321", .eDomestic, "airbusA321"),
+            plane("Boeing 737-800", .eDomestic, "boeing737"),
+            plane("Boeing 777", .eInternational, "boeing777"),
+            plane("Airbus A350", .eInternational, "airbusA350"),
+            plane("Sukhoi Superjet 100", .eDomestic, "sukhoijet")
+        ],
+        "Philippine Airlines": [
+            plane("Boeing 747", .eInternational, "boeing747"),
+            plane("Boeing 777", .eInternational, "boeing777"),
+            plane("Airbus A320", .eDomestic, "airbusA320"),
+            plane("Airbus A321", .eDomestic, "airbusA321"),
+            plane("EJR-195", .eDomestic, "erj195")
+        ],
+        "Qantas": [
+            plane("PC-12", .eDomestic, "pc-12"),
+            plane("King Air 350", .eDomestic, "kingair350")
+        ],
+        "Qatar Airways": [
+            plane("Airbus A320", .eDomestic, "airbusA320"),
+            plane("Airbus A330-300", .eInternational, "airbusA330"),
+            plane("Airbus A350", .eInternational, "airbusA350"),
+            plane("Boeing 777", .eInternational, "boeing777")
+        ],
+        "Saudia Airlines": [
+            plane("Airbus A319", .eDomestic, "airbusA319"),
+            plane("Airbus A320", .eDomestic, "airbusA320"),
+            plane("Airbus A321", .eDomestic, "airbusA321"),
+            plane("Boeing 737-800", .eDomestic, "boeing737"),
+            plane("Boeing 777", .eInternational, "boeing777"),
+            plane("CJR-700", .eDomestic, "crj700"),
+            plane("EJR-175", .eInternational, "ejr175")
+        ],
+        "Scandinavian Airlines": [
+            plane("Airbus A319", .eDomestic, "airbusA319"),
+            plane("Airbus A320", .eDomestic, "airbusA320"),
+            plane("Airbus A321", .eDomestic, "airbusA321"),
+            plane("Boeing 737-800", .eDomestic, "boeing737"),
+            plane("Boeing 777", .eInternational, "boeing777"),
+            plane("CJR-700", .eDomestic, "crj700"),
+            plane("EJR-175", .eInternational, "ejr175")
+        ],
+        "Sichuan Airlines": [
+            plane("Airbus A320", .eDomestic, "airbusA320"),
+            plane("Airbus A330-300", .eInternational, "airbusA330"),
+            plane("Airbus A350", .eInternational, "airbusA350"),
+            plane("Boeing 777", .eInternational, "boeing777")
+        ],
+        "Singapore Airlines": [
+            plane("Airbus A319", .eDomestic, "airbusA319"),
+            plane("Airbus A320", .eDomestic, "airbusA320"),
+            plane("Airbus A321", .eDomestic, "airbusA321"),
+            plane("Boeing 737-800", .eDomestic, "boeing737"),
+            plane("Boeing 777", .eInternational, "boeing777"),
+            plane("CJR-700", .eDomestic, "crj700"),
+            plane("EJR-175", .eInternational, "ejr175")
+        ],
+        "Southwest Airlines": [
+            plane("Airbus A319", .eDomestic, "airbusA319"),
+            plane("Airbus A320", .eDomestic, "airbusA320"),
+            plane("Airbus A321", .eDomestic, "airbusA321"),
+            plane("Boeing 737-800", .eDomestic, "boeing737"),
+            plane("Boeing 777", .eInternational, "boeing777"),
+            plane("CJR-700", .eDomestic, "crj700"),
+            plane("EJR-175", .eInternational, "ejr175")
+        ],
+        "Spirit Airlines": [
+            plane("Airbus A320", .eDomestic, "airbusA320"),
+            plane("Airbus A330-300", .eInternational, "airbusA330"),
+            plane("Airbus A350", .eInternational, "airbusA350"),
+            plane("Boeing 777", .eInternational, "boeing777")
+        ],
+        "Sun Country": [
+            plane("PC-12", .eDomestic, "pc-12"),
+            plane("King Air 350", .eDomestic, "kingair350")
+        ],
+        "Swiss International Air Lines Ltd": [
+            plane("Airbus A330-300", .eInternational, "airbusA330"),
+            plane("Airbus A350", .eInternational, "airbusA350"),
+            plane("Boeing 737-800", .eDomestic, "boeing737")
+        ],
+        "Thomas Cook Airlines": [
+            plane("Airbus A321", .eDomestic, "airbusA321"),
+            plane("Airbus A330-300", .eInternational, "airbusA330"),
+            plane("Airbus A350", .eInternational, "airbusA350"),
+            plane("Boeing 737-800", .eDomestic, "boeing737"),
+            plane("Boeing 777", .eInternational, "boeing777")
+        ],
+        "Turkish Airlines": [
+            plane("787-9 Dreamliner", .eInternational, "dreamliner787"),
+            plane("Boeing 737-800", .eDomestic, "boeing737"),
+            plane("Boeing 777", .eInternational, "boeing777")
+        ],
+        "United Airlines": [
+            plane("Airbus A320", .eDomestic, "airbusA320"),
+            plane("Airbus A321", .eDomestic, "airbusA321"),
+            plane("Airbus A350", .eInternational, "airbusA350"),
+            plane("Airbus A380", .eInternational, "airbusA380"),
+            plane("Boeing 747", .eInternational, "boeing747"),
+            plane("Boeing 767", .eDomestic, "boeing767"),
+            plane("Boeing 777", .eInternational, "boeing777")
+        ],
+        "Virgin Atlantic": [
+            plane("ATR", .eDomestic, "atr"),
+            plane("Airbus A320", .eDomestic, "airbus320"),
+            plane("Airbus A321", .eDomestic, "airbusA321"),
+            plane("Airbus A330-300", .eInternational, "airbusA330")
+        ],
+        "Virgin Australia": [
+            plane("Airbus A320", .eDomestic, "airbusA320"),
+            plane("Airbus A321", .eDomestic, "airbusA321"),
+            plane("Airbus A350", .eInternational, "airbusA350"),
+            plane("Airbus A380", .eInternational, "airbusA380"),
+            plane("Boeing 747", .eInternational, "boeing747"),
+            plane("Boeing 767", .eDomestic, "boeing767"),
+            plane("Boeing 777", .eInternational, "boeing777")
+        ],
+        "Viva Aerobus": [
+            plane("Airbus A320", .eDomestic, "airbusA320"),
+            plane("Airbus A321", .eDomestic, "airbusA321"),
+            plane("Airbus A350", .eInternational, "airbusA350"),
+            plane("Airbus A380", .eInternational, "airbusA380")
+        ],
+        "Volaris": [
+            plane("Airbus A319", .eDomestic, "airbusA319"),
+            plane("Airbus A320", .eDomestic, "airbusA320"),
+            plane("Airbus A321", .eDomestic, "airbusA321"),
+            plane("Airbus A330-300", .eInternational, "airbusA330"),
+            plane("Airbus A350", .eInternational, "airbusA350"),
+            plane("Boeing 737-800", .eDomestic, "boeing737"),
+            plane("Boeing 767", .eDomestic, "boeing767"),
+            plane("EJR-175", .eDomestic, "erj175"),
+            plane("CRJ-200", .eDomestic, "crj200"),
+            plane("CJR-700", .eDomestic, "crj700")
+        ],
+        "West Jet": [
+            plane("ATR", .eDomestic, "atr"),
+            plane("Airbus A320", .eDomestic, "airbus320"),
+            plane("Airbus A321", .eDomestic, "airbusA321"),
+            plane("Airbus A330-300", .eInternational, "airbusA330")
+        ],
+        "Xiamen Airlines": [
+            plane("Airbus A320", .eDomestic, "airbusA320"),
+            plane("Airbus A321", .eDomestic, "airbusA321"),
+            plane("Airbus A350", .eInternational, "airbusA350"),
+            plane("Airbus A380", .eInternational, "airbusA380"),
+            plane("Boeing 747", .eInternational, "boeing747"),
+            plane("Boeing 767", .eDomestic, "boeing767"),
+            plane("Boeing 777", .eInternational, "boeing777")
+        ],
+        "XL Airways France": [
+            plane("Airbus A319", .eDomestic, "airbusA319"),
+            plane("Airbus A320", .eDomestic, "airbusA320"),
+            plane("Airbus A321", .eDomestic, "airbusA321"),
+            plane("Boeing 737-800", .eDomestic, "boeing737"),
+            plane("Boeing 777", .eInternational, "boeing777"),
+            plane("CJR-700", .eDomestic, "crj700"),
+            plane("EJR-175", .eInternational, "ejr175")
         ]
     ]
     return dictionary
