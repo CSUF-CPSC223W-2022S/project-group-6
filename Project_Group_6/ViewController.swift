784422eb1a7dd62c3d851554d656d491b1b07664
Diego Barrales <diego.barrales@csu.fullerton.edu>
2022-04-30 18:41:01 -0700

    created a working tab bar with place holder storyboards.

--- Project_Group_6/ViewController.swift
+++ Project_Group_6/ViewController.swift
@@ -8,12 +8,10 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var Image: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
 }
-
