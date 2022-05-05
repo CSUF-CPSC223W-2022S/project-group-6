f9dc3abf117374ea28138ec6c2eb3dd8c1a4fd97
Diego Barrales <diego.barrales@csu.fullerton.edu>
2022-05-05 09:12:54 -0700

    Update UI and fixed offline version

--- Project_Group_6/ViewController.swift
+++ Project_Group_6/ViewController.swift
@@ -35,8 +35,6 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        cityName.text = "Los Angeles"
-        Image.image = UIImage(named: "LA-starting-Image")
     }
 
     override func viewWillAppear(_ animated: Bool) {
